c43c1b97432bb3c30a8442dc4f1fc62f7696ab48
Ugur CANGAL <ugurcangal@gmail.com>
2024-07-10 08:37:18 +0300

    some css changed(Post)

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,17 +1,20 @@
 import { PostType } from "../types/Types"
 import "../css/Post.css"
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 interface PostProps{
     postProps: PostType
 }
 
 const Post = ({postProps}: PostProps) => {
-  const {id,postText,date} = postProps;
-
-
+  const {postText,date} = postProps;
+  const user = useSelector((state: RootState) => state.auth.user);
+  
   return (
     <div className="container">
-      <div className="post">{postText}</div>
+      <div className="username">@anon-{user.uid}</div>
+      <div className="post-text">{postText}</div>
       <div className="post-date">{date}</div>
     </div>
   )
